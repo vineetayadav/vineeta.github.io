f8ce0e6bdbe17300f169cf0349cdbfd53b2b4245
Priyanshuydv2003 <92259252+Priyanshuydv2003@users.noreply.github.com>
2022-01-21 23:06:39 +0530

    ri

---
+++ contant/js/rmb.js
@@ -0,0 +1,215 @@
+// copyright by trusteepartner.in 
+//author - Priyanshu yadav
+// for more help contact - +91 6350029208
+
+//servisec > listing > rd mr 1st btn
+function rmbl1() {
+  var dots = document.getElementById("d3l1");
+  var moreText = document.getElementById("mtl1");
+  var btnText = document.getElementById("mbl1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+//servisec > listing > rd mr 2nd btn
+function rmbl2() {
+    var dots = document.getElementById("d3l2");
+    var moreText = document.getElementById("mtl2");
+    var btnText = document.getElementById("mbl2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+//servisec > listing > rd mr 3rd btn
+  function rmbl3() {
+    var dots = document.getElementById("d3l3");
+    var moreText = document.getElementById("mtl3");
+    var btnText = document.getElementById("mbl3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+
+//servisec > home > rd mr 1st btn
+function rmh1() {
+    var dots = document.getElementById("rmh1");
+    var moreText = document.getElementById("mth1");
+    var btnText = document.getElementById("mbh1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+//servisec > home > rd mr 2nd btn
+function rmh2() {
+    var dots = document.getElementById("rmh2");
+    var moreText = document.getElementById("mth2");
+    var btnText = document.getElementById("mbh2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+//servisec > home > rd mr 3rd btn
+function rmh3() {
+    var dots = document.getElementById("rmh3");
+    var moreText = document.getElementById("mth3");
+    var btnText = document.getElementById("mbh3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+//servisec > about > rd mr 1st btn
+function rmbab1() {
+    var dots = document.getElementById("d3ab1");
+    var moreText = document.getElementById("mtab1");
+    var btnText = document.getElementById("mbab1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  //servisec > about > rd mr 2nd btn
+  function rmbab2() {
+      var dots = document.getElementById("d3ab2");
+      var moreText = document.getElementById("mtab2");
+      var btnText = document.getElementById("mbab2");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+  
+  //servisec > about > rd mr 3rd btn
+    function rmbab3() {
+      var dots = document.getElementById("d3ab3");
+      var moreText = document.getElementById("mtab3");
+      var btnText = document.getElementById("mbab3");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+  
+  
+  //servisec > oth > rd mr 1st btn
+  function rmo1() {
+      var dots = document.getElementById("rmo1");
+      var moreText = document.getElementById("mto1");
+      var btnText = document.getElementById("mbo1");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+  
+  //servisec > oth > rd mr 2nd btn
+  function rmo2() {
+      var dots = document.getElementById("rmo2");
+      var moreText = document.getElementById("mto2");
+      var btnText = document.getElementById("mbo2");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+  
+  //servisec > oth > rd mr 3rd btn
+  function rmo3() {
+      var dots = document.getElementById("rmo3");
+      var moreText = document.getElementById("mto3");
+      var btnText = document.getElementById("mbo3");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+
+
+
+
+
+  //read more and less button js
